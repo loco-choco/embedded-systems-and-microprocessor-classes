d004231279bbf1e921e2caf0ddb764ad3ce2e124
loco-choco <contact@locochoco.dev>
2024-10-07 09:06:46 -0300

    added user input to control the timer speeds

--- microprocessors/project-1/timer.asm
+++ microprocessors/project-1/timer.asm
@@ -20,6 +20,7 @@ main:
   ; Resetar todas as variaveis
   mov r0, #0
   mov r1, #0
+  mov r2, #0
   mov r3, #0
   mov r4, #0
   mov r5, #0
@@ -41,10 +42,38 @@ habilitar_interrupt_timer_0:
   ; Loop para ligar e alterar modo do timer
   ; dependendo das chaves SW0 (p2.0) e SW1 (p2.1)
 input_loop:
+  ; se sw0 mudar de status, habilitar modo rapido
+  ; se sw1 mudar de status, habilitar modo lento
 check_sw0:
+  mov a, r4 ; armazenar valor antigo do status de sw0 para referencia
+  jb p2.0, sw0_disabled
+sw0_enabled:
+  mov r4, #1 ; atualiza que sw0 esta ativado
+  cjne a, #0, check_sw1
+  jmp enable_slow_mode
+sw0_disabled:
+  mov r4, #0 ; atualiza que sw0 esta desativado
+  cjne a, #1, check_sw1
+enable_slow_mode:
+  mov r2, #1 ; se mudou, ativar o clock
+  mov r3, #0 ; colocar em modo lentro
 check_sw1:
+  mov a, r5 ; armazenar valor antigo do status de sw1 para referencia
+  jb p2.1, sw1_disabled
+sw1_enabled:
+  mov r5, #1 ; atualiza que sw1 esta ativado
+  cjne a, #0, loop_end
+  jmp enable_fast_mode
+sw1_disabled:
+  mov r5, #0 ; atualiza que sw1 esta desativado
+  cjne a, #1, loop_end
+enable_fast_mode:
+  mov r2, #1 ; se mudou, ativar o clock
+  mov r3, #1 ; colocar em modo rapido
+loop_end:
   jmp input_loop
 timer0_int_body:
+  push ACC
   cjne r2, #1, resetar_timer ; se nao estiver ligado, voltar a contagem do timer
 contagem_ligada:
   inc r1
@@ -73,5 +102,6 @@ resetar_timer:
   mov tl0, #00h
   mov th0, #86h
   setb tr0 ; habilita timer 0
+  pop ACC
   reti
   
